ca7c367e7b89e779874d286a6b537729e1fc832e
Levansarajishvili <levan.sarajishvili123@gmail.com>
2026-06-19 17:30:48 +0400

    project section continue

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,8 +6,8 @@ export const HeroSection = () => {
     className="relative min-h-screen flex flex-col items-center justify-center px-4"
     >
         <div className="container max-w-4xl mx-auto text-center z-10">
-           <div className="space-y-6">
-             <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+           <div className="space-y-6 pt-14">
+             <h1 className="text-1xl md:text-6xl font-bold tracking-tight">
                 <span className="opacity-0 animate-fade-in">Hi, I'am </span>
                 <span className="text-primary opacity-0 animate-fade-in-delay-1">Levan</span>
                 <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">Sarajishvili </span>
